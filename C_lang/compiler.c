ba50781df883494b91e4ce11d5e3a3faa92b2737
mega-317 <mega31712@gmail.com>
2024-04-09 23:41:57 +0900

    compile 완성, 가다듬기만 하면 될듯

--- C_lang/compiler.c
+++ C_lang/compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX 100
 
@@ -38,15 +39,19 @@ int rhs_len[] = { 0, 3, 1, 3, 1, 3, 1 };
 char token[] = { 'd', '+', '*', '(', ')', '$' };
 char NT[] = { ' ', 'E', 'T', 'F' };
 int stack[MAX];
-int sp = 0;
-int state = 0;
-int step = 1;
+//int sp = 0;
+int sp;
+//int state = 0;
+int state;
+int step;
+//int inputPop = 0;
+int inputPop;
 
 int LR_Parser(char* input);
 
-int LR_Parser_Shift(char token, int* state, int* sp, int num);
-int LR_Parser_Reduce(char token, int* state, int* sp, int num);
-void Print(int* sp, char* op, int num);
+int LR_Parser_Shift(char token, int* state, int* sp, int num, char* input);
+int LR_Parser_Reduce(char token, int* state, int* sp, int num, char* input);
+void Print(int* sp, char* op, int num, char* input);
 
 int main() {
     char input[MAX];
@@ -62,9 +67,13 @@ int main() {
 }
 
 int LR_Parser(char* input) {
+    step = 1;
+    sp = 0;
+    state = 0;
+    inputPop = 0;
     stack[sp] = 0;
 
-    printf("(%d) initial : %d\n", step, stack[sp]);
+    printf("(%d) initial : %d %s\n", step, stack[sp], input);
     step += 1;
 
     char *string = NULL; // string 포인터에 입력값 지정
@@ -92,96 +101,99 @@ int LR_Parser(char* input) {
         }
 
         if ( j == 6 ) { // 입력값이 토큰이 아닐 경우
-            printf("invalid token\n");
+            printf("(%d) invalid token (%c) error\n", step, string[i]);
             return -1;
         }
 
         if ((action_tbl[state][j] > 0) && (action_tbl[state][j] < 20)) {
-            LR_Parser_Shift(string[i], &state, &sp, j);
+            LR_Parser_Shift(string[i], &state, &sp, j, input);
         } else if (action_tbl[state][j] < 0) {
-            LR_Parser_Reduce(string[i], &state, &sp, j);
+            LR_Parser_Reduce(string[i], &state, &sp, j, input);
         } else if (action_tbl[state][j] == 999) {
             printf("(%d) accept\n", step);
             return 0;
         } else {
-            printf("error\n");
+            printf("(%d) error\n", step);
             return -1;
         }
 
     }
 }
 
-int LR_Parser_Reduce(char token, int* state, int* sp, int num) {
+int LR_Parser_Reduce(char token, int* state, int* sp, int num, char* input) {
 
     int* state_1 = state;
     int* sp_1 = sp;
 
     int order = -1 * (action_tbl[*state_1][num]); // order: reduce 몇번 명령인지
-        
-    // for (int m = 0; m < rhs_len[order] ; m++) { // rhs의 크기 만큼 스택 pop
-    //     *sp_1 -=1;
-    // }
 
-    if (rhs_len[order] == 1) { // 여기는 수정이 필요해보임
-        *sp_1 -=1;
-    } else if (rhs_len[order] == 3) {
-        *sp_1 -=5;
+    // if (rhs_len[order] == 1) { // 여기는 수정이 필요해보임
+    //     *sp_1 -=1;
+    // } else if (rhs_len[order] == 3) {
+    //     *sp_1 -=5;
+    // }
+    
+    int count = 0;
+    while (count < rhs_len[order]) {
+        if (stack[*sp_1] > 11) {
+            *sp_1 -= 1;
+            count += 1;
+        } else {
+            *sp_1 -= 1;
+        }
     }
+    *sp_1 += 1; // ** 포인터이기 때문에 *sp_1++ 를 하면 안된다
 
     stack[*sp_1] = lhs[order];
 
     switch(stack[*sp_1]) {
         case 'E':
-        if (goto_tbl[stack[*sp_1-1]][1] != 0) {
-            *sp_1 += 1;
-            stack[*sp_1] = goto_tbl[stack[*sp_1-2]][1];
-            break;
-        }
-        else {
-            printf("error\n");
-            return -1;
-        }
+            if (goto_tbl[stack[*sp_1-1]][1] != 0) {
+                *sp_1 += 1;
+                stack[*sp_1] = goto_tbl[stack[*sp_1-2]][1];
+                break;
+            }
+            else {
+                printf("(%d) error\n", step);
+                return -1;
+            }
         case 'T':
-        if (goto_tbl[stack[*sp_1-1]][2] != 0) {
-            *sp_1 += 1;
-            stack[*sp_1] = goto_tbl[stack[*sp_1-2]][2];
-            break;
-        } else {
-            printf("error\n");
-            return -1;
-        }
+            if (goto_tbl[stack[*sp_1-1]][2] != 0) {
+                *sp_1 += 1;
+                stack[*sp_1] = goto_tbl[stack[*sp_1-2]][2];
+                break;
+            } else {
+                printf("(%d) error\n", step);
+                return -1;
+            }
         case 'F':
             if (goto_tbl[stack[*sp_1-1]][3] != 0) {
                 *sp_1 += 1;
                 stack[*sp_1] = goto_tbl[stack[*sp_1-2]][3];
                 break;
             } else {
-                printf("error\n");
+                printf("(%d) error\n", step);
                 return -1;
             }
     }
 
     *state_1 = stack[*sp_1]; // 상태값 갱신
 
-    // for (int m = 0; m < *sp_1 + 1; m++) {
-    //     printf("stack result is %d\n", stack[m]);
-    // }
-
-    Print(sp_1, "reduce", order);
+    Print(sp_1, "reduce", order, input);
 
     if (action_tbl[*state_1][num] < 0) {
-        LR_Parser_Reduce(token, state_1, sp_1, num);
+        LR_Parser_Reduce(token, state_1, sp_1, num, input);
     } else if ((action_tbl[*state_1][num] > 0) && (action_tbl[*state_1][num] < 999)) {
-        LR_Parser_Shift(token, state_1, sp_1, num);
+        LR_Parser_Shift(token, state_1, sp_1, num, input);
     } else if (action_tbl[*state_1][num] == 999) {
         printf("(%d) accept\n", step);
     } else {
-        printf("error2\n");
+        printf("(%d) error\n", step);
         return -1;
     }
 }
 
-int LR_Parser_Shift(char token, int* state, int* sp, int num) {
+int LR_Parser_Shift(char token, int* state, int* sp, int num, char* input) {
 
     int* state_1 = state;
     int* sp_1 = sp;
@@ -192,13 +204,16 @@ int LR_Parser_Shift(char token, int* state, int* sp, int num) {
     *sp_1 += 1;
     stack[*sp_1] = *state_1;
 
-    // for (int m = 0; m < *sp_1 + 1; m++) {
-    //     printf("stack result is %d\n", stack[m]);
-    // }
-    Print(sp_1, "shift", *state_1);
+    Print(sp_1, "shift", *state_1, input);
 }
 
-void Print(int* sp, char* op, int num) {
+void Print(int* sp, char* op, int num, char* input) {
+    if(strcmp(op, "shift") == 0) {
+        inputPop++;
+    }
+
+    input = &input[inputPop];
+    
     printf("(%d) %s %d: ", step, op, num);
     for(int i = 0; i<*sp + 1; i++) {
         if (stack[i] > 11) {
@@ -207,6 +222,8 @@ void Print(int* sp, char* op, int num) {
             printf("%d", stack[i]);
         }
     }
+    printf(" %s", input);
     printf("\n");
     step += 1;
 }
+
