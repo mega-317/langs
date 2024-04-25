e250011e0839ac784c7f3edb787c46389a7c3b93
mega-317 <mega31712@gmail.com>
2024-04-25 23:14:48 +0900

    반드시 set(), dictionary 공부

--- Python/14425.py
+++ Python/14425.py
@@ -1,22 +1,17 @@
 n, m = map(int, input().split())
-arr1 = []
+arr1 = set()
 arr2 = []
 
 for i in range(n):
     a = input()
-    arr1.append(a)
+    arr1.add(a)
     
 for i in range(m):
     b = input()
     arr2.append(b)
 
-arr1 = sorted(arr1, key=len)
-
 count = 0
-for i in range(m):
-    for j in range(n):
-        if arr1[j] == arr2[i]:
-            count += 1
-        if len(arr1[j]) < len(arr2[i]):
-            continue
+for element in arr2:
+    if element in arr1:
+        count += 1
 print(count)
