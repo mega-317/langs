963f6bc91f7f639d4a2a0189cacab4657bed1cbd
mega-317 <mega31712@gmail.com>
2024-05-06 23:21:29 +0900

    리액트 공부

--- JavaScript/React/shop/src/App.js
+++ JavaScript/React/shop/src/App.js
@@ -1,25 +1,76 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button, Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
+import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.js';
 
 function App() {
+
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
+
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg'></div>
+            <div className='container'>
+              <div className='row'>
+                {
+                  shoes.map((a, i)=>{
+                    return (
+                      <Card shoes={a} index={i+1}/>
+                    )  
+                  })
+                }
+              </div>
+            </div>
+          </>
+        }/>
         
-      </div>
+        <Route path="/detail" element={ <Detail />}/>
+        <Route path="/about" element={ <About /> }>
+          <Route path="member" element={ <div>멤버임</div> } />
+          <Route path="location" element={ <div>위치정보임</div> } />
+        </Route>
+        <Route path="*" element={ <div>404 error</div> } />
+      </Routes>
+
     </div>
   );
+}
+
+function About(){
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Card(props){
+  return (
+    <div className='col-md-4'>
+      <img src={`https://codingapple1.github.io/shop/shoes${props.index}.jpg`} width="80%"/>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
